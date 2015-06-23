fba4a10eb8675eafa08b814179a59c38aa6424ce
stuland-team <developer_team@stuland.ir>
2015-06-23 21:16:26 +0430

    this is new user

--- app/src/main/java/stuland/app/stulandactivity/Stuland2ndActivity.java
+++ app/src/main/java/stuland/app/stulandactivity/Stuland2ndActivity.java
@@ -12,6 +12,8 @@ public class Stuland2ndActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stuland2nd);
+
+        //this is test line
     }
 
 
